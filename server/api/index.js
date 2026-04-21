5592eafda48dc9f4be53ee39de3a5c848020d6e5
Salman Inayat <salmaninayat1999@gmail.com>
2026-04-21 08:38:34 +0500

    server test

--- server/api/index.js
+++ server/api/index.js
@@ -1,2 +1,13 @@
-const app = require('../dist/index.js');
-module.exports = app.default || app;
+let app;
+try {
+  const mod = require('../dist/index.js');
+  app = mod.default || mod;
+} catch (err) {
+  app = (req, res) => {
+    res.status(500).json({
+      error: err.message,
+      stack: err.stack,
+    });
+  };
+}
+module.exports = app;
